2a111d9092f0cd1801c2ad061886826e9d2afe85
Raghul <raghul.gtg23@gmail.com>
2024-03-12 12:48:14 +0000

    corrected previous commit

--- focalboard/app/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Focalboard in the background
-/opt/focalboard/bin/focalboard-server --config /opt/focalboard/config.json &
-
-# Start NGINX in the foreground
-nginx -g "daemon off;" 
